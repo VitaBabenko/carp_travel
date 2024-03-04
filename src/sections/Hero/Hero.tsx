96d17715d00520333080685c24cc3734662e95c2
Vita Babenko <babenkovita1988@gmail.com>
2024-03-04 16:38:11 +0200

    hero in process

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,3 +1,30 @@
+import data from "@/data/common.json";
+
+import styles from "./Hero.module.css";
+
+const { spanHero, title, list, description, btnHero } = data.hero;
+
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={`${styles.bg_section} h-full w-full overflow-hidden`}>
+      <div className="container mx-auto">
+        <div>
+          <span> {spanHero.first}</span>
+          <span>{spanHero.second}</span>
+        </div>
+        <p>{spanHero.third}</p>
+        <h1>
+          <span>{title.firstTitle}</span>
+          {title.secondTitle}
+        </h1>
+        <ul>
+          {list.map(({ id, label }) => (
+            <li key={id}>{label}</li>
+          ))}
+        </ul>
+        <p>{description}</p>
+        <button type="button">{btnHero}</button>
+      </div>
+    </section>
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,32 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "320px",
+      md: "768px",
+      lg: "1280px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        padding: {
+          sm: "1.25rem",
+          md: "2rem",
+        },
+      },
+      colors: {
+        "main-color": "#FFF",
+        "error-color": "#FF5757",
+      },
+      transitionDuration: {
+        default: "250ms",
+      },
+      transitionTimingFunction: {
+        default: "cubic-bezier(0.3, 0, 0.2, 1)",
       },
     },
   },
